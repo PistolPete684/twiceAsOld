a285d99015a301df4ff637626fc3c833bcbfdd1c
Peter <pwlinton@mta.ca>
2023-06-04 20:33:33 -0300

    adding js file

---
+++ app.js
@@ -0,0 +1,9 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // setup your variable to hold the difference between dad and double the sons age
+   //make sure the number is an absolute number
+  let numYearsOffDouble = Math.abs(dadYearsOld - (sonYearsOld * 2));
+  
+  
+  //return the difference being stored in the variable
+  return numYearsOffDouble;
+}
